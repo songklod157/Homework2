81e406d62f25ea72f6b9c05fa2c1c678226262e6
Nichkamon Promputta <fainichkamon@gmail.com>
2020-08-29 21:15:27 +0700

    fix controller and add model

--- Homework2/Controllers/Order.cs
+++ Homework2/Controllers/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Homework2.Models;
 
 namespace Homework2.Controllers
 {
@@ -14,15 +15,31 @@ namespace Homework2.Controllers
             var fullname = HttpContext.Request.Form["name"].ToString();
             ViewBag.name = fullname;
 
-            var game = HttpContext.Request.Form["game"].ToString();
-            int Q = Convert.ToInt32(HttpContext.Request.Form["qty"].ToString());
-            int P = Convert.ToInt32(HttpContext.Request.Form["pri"].ToString());
-            int sum = Q * P;
-            ViewBag.qty = Q.ToString();
-            ViewBag.pri = P.ToString();
-            ViewBag.namegame = game;
-            ViewBag.total = sum.ToString();
-            return View();
+            List<ListData> PureList = new List<ListData> {
+                new ListData{game="Far Cry 5",price= 620},
+                new ListData{game="Gta 5",price= 550},
+                new ListData{game="Dota 2",price= 360},
+                new ListData{game="Valorant",price= 130},
+                new ListData{game="Monter Hunter World",price= 480},
+                new ListData{game="Assassin's Creed",price= 510},
+                new ListData{game="Tom Clancys",price= 610},
+                new ListData{game="Doom",price= 220},
+                new ListData{game="Division",price= 145},
+                new ListData{game="Bettlefield",price= 310},
+                new ListData{game="Just Cause",price= 210},
+                new ListData{game="Fall Guys",price= 510},
+                new ListData{game="Watch Dogs2",price= 650},
+                new ListData{game="Over Wacth",price= 430},
+                new ListData{game="Devil Maycry5",price= 220},
+                };
+
+            var Result = new List<ListData>();
+
+            for (int i = 0; i < PureList.Count; i++)
+            {
+                Result.Add(new ListData { game = PureList[i].game, price = PureList[i].price });
+            }
+            return View(Result);
         }
 
         public IActionResult Login()
---
+++ Homework2/Models/ListData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Homework2.Models
+{
+    public class ListData
+    {
+        public string game { get; set; }
+        public int price { get; set; }
+    }
+}
