2bc3bc35bef5ebd242ffaf712570ce48fe9ff518
Nichkamon Promputta <fainichkamon@gmail.com>
2020-08-31 01:05:11 +0700

    update bug

--- Homework2/Controllers/Order.cs
+++ Homework2/Controllers/Order.cs
@@ -15,43 +15,35 @@ namespace Homework2.Controllers
             var fullname = HttpContext.Request.Form["name"].ToString();
             ViewBag.name = fullname;
 
-            List<ListData> PureList = new List<ListData> {
-                new ListData{game="Far Cry 5",price= 620},
-                new ListData{game="Gta 5",price= 550},
-                new ListData{game="Dota 2",price= 360},
-                new ListData{game="Valorant",price= 130},
-                new ListData{game="Monter Hunter World",price= 480},
-                new ListData{game="Assassin's Creed",price= 510},
-                new ListData{game="Tom Clancys",price= 610},
-                new ListData{game="Doom",price= 220},
-                new ListData{game="Division",price= 145},
-                new ListData{game="Bettlefield",price= 310},
-                new ListData{game="Just Cause",price= 210},
-                new ListData{game="Fall Guys",price= 510},
-                new ListData{game="Watch Dogs2",price= 650},
-                new ListData{game="Over Wacth",price= 430},
-                new ListData{game="Devil Maycry5",price= 220},
-                };
+            var listgame = new List<ListData>();
+            listgame.Add(new ListData { game = "Far Cry 5", price = 620 });
+            listgame.Add(new ListData { game="Gta 5",price= 550});
+            listgame.Add(new ListData { game="Dota 2",price= 360});
+            listgame.Add(new ListData { game="Valorant",price= 130});
+            listgame.Add(new ListData { game="Monter Hunter World",price= 480});
+            listgame.Add(new ListData { game="Assassin's Creed",price= 510});
+            listgame.Add(new ListData { game="Tom Clancys",price= 610});
+            listgame.Add(new ListData { game="Doom",price= 220});
+            listgame.Add(new ListData { game="Division",price= 145});
+            listgame.Add(new ListData { game="Bettlefield",price= 310});
+            listgame.Add(new ListData { game="Just Cause",price= 210});
+            listgame.Add(new ListData { game="Fall Guys",price= 510});
+            listgame.Add(new ListData { game="Watch Dogs2",price= 650});
+            listgame.Add(new ListData { game="Over Wacth",price= 430});
+            listgame.Add(new ListData { game="Devil Maycry5",price= 220});
 
-            var Result = new List<ListData>();
-
-            for (int i = 0; i < PureList.Count; i++)
-            {
-                Result.Add(new ListData { game = PureList[i].game, price = PureList[i].price });
-            }
-            return View(Result);
+            return View(listgame);
         }
 
         public IActionResult Login()
         {
             return View();
         }
-        public IActionResult Getname()
+        public IActionResult order()
         {
-            var fullname = HttpContext.Request.Form["name"].ToString();
-            ViewBag.name = fullname;
-            return View("Login");
+            return View();
         }
+
         [HttpPost]
         public IActionResult Buy()
         {
@@ -63,7 +55,13 @@ namespace Homework2.Controllers
             ViewBag.pri = P.ToString();
             ViewBag.namegame = game;
             ViewBag.total = sum.ToString();
-            return View("Index");
+            return View("order");
+        }
+        public IActionResult Getname()
+        {
+            var fullname = HttpContext.Request.Form["name"].ToString();
+            ViewBag.name = fullname;
+            return View("Login", "Index");
         }
     }
 }
